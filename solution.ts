f0c2bfdef3205c03c64a5d30562e2038dd866b51
Shahriar Ahmed ~ SHIFAT <shahriar.ahmed.7878@gmail.com>
2025-11-17 08:31:30 +0600

    Implement Person class with constructor and getDetails method

--- solution.ts
+++ solution.ts
@@ -21,3 +21,23 @@ const getLength = <T>(values: T[] | string): number | undefined => {
 };
 // console.log(getLength("typescript"));
 // console.log(getLength([10, 20, 30, 40]));
+
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  getDetails() {
+    return `'Name:${this.name}, Age:${this.age}'`;
+  }
+}
+
+// const person1 = new Person("John Doe", 30);
+// console.log(person1.getDetails());
+
+// const person2 = new Person("Alice", 25);
+// console.log(person2.getDetails());
